1c23f07132a2f85bfdd405769e47d7c46d377c7f
Neel Patel <neelpatelbiz@gmail.com>
2025-04-15 14:38:34 -0400

    mul and add example

---
+++ egg/ilp/egraph/README.md
@@ -0,0 +1,2 @@
+ 2019  cargo run .
+ 2020  dot -Tpdf egraph.dot -o egraph.pdf
--- egg/ilp/egraph/src/main.rs
+++ egg/ilp/egraph/src/main.rs
@@ -5,7 +5,9 @@ fn main() {
     define_language! {
         enum SimpleLang {
             "+" = Add([Id; 2]),
+            "*" = Mul([Id; 2]),
             "a" = A,
+            "b" = B,
         }
     }
 
@@ -14,17 +16,21 @@ fn main() {
 
     // Add initial expressions
     let a = egraph.add(SimpleLang::A);
-    let add_a_a = egraph.add(SimpleLang::Add([a, a]));
+    let b = egraph.add(SimpleLang::B);
+    let add_a_b = egraph.add(SimpleLang::Add([a, b]));
+    let add_b_a = egraph.add(SimpleLang::Add([b, a]));
+    let mul_add_a_b_add_b_a = egraph.add(SimpleLang::Mul([add_a_b, add_b_a]));
+    let add_mul_add_a_b_add_b_a_mul_add_a_b_add_b_a = egraph.add(SimpleLang::Add([mul_add_a_b_add_b_a, mul_add_a_b_add_b_a]));
 
-    // Define rewrites that create a loop from child to parent
     let rules: &[Rewrite<SimpleLang, ()>] = &[
-        rw!("add-to-self"; "(+ ?x ?x)" => "?x"),
-        rw!("self-to-add"; "?x" => "(+ ?x ?x)"),
+        rw!("commute-add"; "(+ ?x ?y)" => "(+ ?y ?x)"),
+        rw!("commute-mul"; "(* ?x ?y)" => "(* ?y ?x)"),
     ];
 
     // Run the rewrites
     let runner = Runner::default().with_egraph(egraph).run(rules);
 
-    // Print the e-graph to observe the loop
+
     runner.egraph.dot().to_dot("egraph.dot").unwrap();
+
 }
