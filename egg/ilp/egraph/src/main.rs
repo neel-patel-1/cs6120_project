e085fc154b42d56b04fe4082b8c56e8e783a43de
Neel Patel <neelpatelbiz@gmail.com>
2025-04-15 14:03:41 -0400

    simle egraph example

--- egg/ilp/egraph/src/main.rs
+++ egg/ilp/egraph/src/main.rs
@@ -1,9 +1,32 @@
-use good_lp::{variables, variable };
+use egg::{*, rewrite as rw};
 
 fn main() {
+    // Define the language
+    define_language! {
+        enum SimpleLang {
+            "+" = Add([Id; 2]),
+            "a" = A,
+            "b" = B,
+        }
+    }
 
-    let mut vars = variables!();
-    vars.add(variable().binary());
+    // Create an empty e-graph
+    let mut egraph: EGraph<SimpleLang, ()> = EGraph::default();
 
+    // Add initial expressions
+    let a = egraph.add(SimpleLang::A);
+    let b = egraph.add(SimpleLang::B);
+    let add_ab = egraph.add(SimpleLang::Add([a, b]));
 
+    // Define rewrites that create a loop
+    let rules: &[Rewrite<SimpleLang, ()>] = &[
+        rw!("add-commute"; "(+ ?x ?y)" => "(+ ?y ?x)"),
+        rw!("add-self"; "(+ ?x ?x)" => "?x"),
+    ];
+
+    // Run the rewrites
+    let runner = Runner::default().with_egraph(egraph).run(rules);
+
+    // Print the e-graph to observe the loop
+    runner.egraph.dot().to_dot("egraph.dot").unwrap();
 }
